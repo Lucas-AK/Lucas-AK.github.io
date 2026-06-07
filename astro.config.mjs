98bcf22563f65497fb0665e43740fabfc60d9043
Lucas Kock <github@lucasak.dev>
2026-06-07 22:21:55 +0200

    Remove tailwind, clean up dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config'
 
-import tailwind from '@astrojs/tailwind';
-
 export default defineConfig({
   site: 'https://lucas-ak.github.io/',
-  integrations: [tailwind()],
 })
